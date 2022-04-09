893f47ba1333ba72fc2ad19927c5e155949ac6fb
Evan Hall <ehall9@student00.cse.nd.edu>
2022-04-09 14:50:26 -0400

    Main File Created

---
+++ src/vpp.cpp
@@ -0,0 +1,12 @@
+/*************************
+ * Authors: Matty's Merry Militia
+ * File Name: vpp.cpp
+ * Date created: 4/9/2022
+ * File Contents: This file contains the main function
+*************************/
+
+#include "../include/vpp.h"
+
+int main(){
+	return 0;
+}
