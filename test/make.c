e240a672e6f3c7b6c05a31ee67950d50b1b16d19
Jeremy Stevens <jsteve22@student11.cse.nd.edu>
2022-04-26 11:05:44 -0400

    Line Numbers for each file

--- test/make.c
+++ test/make.c
@@ -1,21 +1,21 @@
-//teeeeest        
-        
-#include <stdio.h>        
- 
-// this is a test to see if 
-// we can add new lines 
-// the answer is yes we can 
- 
-int main()        
-{  
-	int i;  
-	for (i = 0; i < 5; i++) {        
-		fprintf(stdout,"%d\n",i);        
-	}        
-	return 0;        
-}        
-        
-        
-        
-// testing comments and saving line        
-        
+//teeeeest
+
+#include <stdio.h>
+
+// this is a test to see if
+// we can add new lines
+// the answer is yes we can
+
+int main()
+{
+	int i;
+	for (i = 0; i < 5; i++) {
+		fprintf(stdout,"%d\n",i);
+	}
+	return 0;
+}
+
+
+
+// testing comments and saving line
+
